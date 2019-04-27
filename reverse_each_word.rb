99fc2da1596ddcbfd7d8f59c738f0cc9a197465d
Alex Velasquez <jtinnesco@gmail.com>
2019-04-27 04:01:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split(" ")
-  reverse_sentence = new_sentence
+  reverse_sentence = new_sentence.collect do {}
   sentence.collect do |word| 
     reversed << word.reverse 
   end
