082e81c6965ddcac21ebb5c0161b60364034ae0c
Alex Velasquez <jtinnesco@gmail.com>
2019-04-27 04:08:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
-  new_array = []
+  reversed = []
   array = sentence.split(" ")
   array.collect do |word|
-    new_array << word.reverse 
+    reversed << word.reverse 
   end
-  new_array.join(" ")
+  reversed.join(" ")
 end
